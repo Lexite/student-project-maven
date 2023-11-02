33f3f8225bdb0657afc23b413042295485a9cfd8
Alexei Rusnac <alexeirusnac1997@gmail.com>
2023-11-02 14:06:36 +0200

    Lesson67_Junit

---
+++ src/main/java/edu/javacourse/Main.java
@@ -0,0 +1,7 @@
+package edu.javacourse;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/edu/javacourse/studentorder/dao/DictionaryDaoImplTest.java
@@ -0,0 +1,26 @@
+package edu.javacourse.studentorder.dao;
+
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class DictionaryDaoImplTest {
+
+    @Test
+    public void testExample1(){
+        System.out.println("TEST 1");
+    }
+
+    @Test
+    @Ignore
+    public void testExample2(){
+        System.out.println("TEST 2");
+    }
+
+    @Test
+    public void testExample3(){
+        System.out.println("TEST 3");
+        throw new RuntimeException("BAD RESULT");
+    }
+
+}
